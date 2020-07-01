f8ec5db11502b1c49226e7bd0c4e4c955fa8fe28
wuyou <55686695+shiwun@users.noreply.github.com>
2020-07-01 14:19:20 +0800

    测试git提交

---
+++ src/com/tbyf/test/TestGit.java
@@ -0,0 +1,15 @@
+package com.tbyf.test;
+
+/**
+ * 用来测试git
+ *
+ * @author wuy
+ * @date 2020/7/1
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("this project will be sent to git.");
+    }
+
+}
